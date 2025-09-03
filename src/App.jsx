63c0f33916048b9379a9171975ea6b0d9090e45f
amy-verma <amitjsr8786@gmail.com>
2025-09-03 17:32:42 +0530

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+
+const App=()=>{
+  const fetchData=async()=>{
+    try{
+      const response=await fetch("https://jsonplaceholder.typicode.com/users");
+      const data=await response.json();
+      console.log(data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+  useState(()=>{
+    fetchData()
+  },[])
+  return (<>
+  </>)
+}
+export default App
 // import { useEffect, useState } from "react"
 
 // const App=()=>{
